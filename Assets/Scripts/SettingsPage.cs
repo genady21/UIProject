2f555a0d1baad7f199f3195dc727e3fbc11eea57
Valery <wazgenby@gmail.com>
2024-02-13 02:52:33 +0100

    add

--- Assets/Scripts/SettingsPage.cs
+++ Assets/Scripts/SettingsPage.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
 
 public class SettingsPage : Page
 {
+    [SerializeField] private Image Bg;
     public override void Show()
     {
         base.Show();
@@ -14,5 +17,10 @@ public class SettingsPage : Page
     {
         base.Close();
         Debug.Log("Close SettingsPage");
+    }
+
+    public void ChangeColorBG()
+    {
+        Bg.color=Color.blue;
     }
 }
--- Assets/Scripts/ShopPage.cs
+++ Assets/Scripts/ShopPage.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopPage : Page
 {
+    [SerializeField] private Text _currency;
     public override void Show()
     {
         base.Show();
         Debug.Log("Open ShopPage");
-        
+        _currency.text = "20";
+
     }
 
     public override void Close()
@@ -16,5 +19,9 @@ public class ShopPage : Page
         base.Close();
         Debug.Log("Close ShopPage");
     }
-    
+
+    public void Buy()
+    {
+        _currency.text = "0";
+    }
 }
